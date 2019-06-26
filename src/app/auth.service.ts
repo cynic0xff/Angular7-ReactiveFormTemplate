865c46b9d71b0c85abcce08861ca2526057e112f
Scott Wren <cynics0xff@gmail.com>
2019-06-26 16:29:52 -0400

    Add login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,16 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  public login(userInfo: User) {
+    localStorage.setItem('ACCESS_TOKEN', "access_token");
+  }
+
+  public isLoggedIn() {
+    return localStorage.getItem('ACCESS_TOKEN') !== null;
+  }
+
+  public logout() {
+    localStorage.removeItem('ACESS_TOKEN');
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  isSubmitted  =  false;
+  get formControls() { return this.loginForm.controls; }
+
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+
+    this.isSubmitted = true;
+    
+    if(this.loginForm.invalid)
+      return;
+
+    this.authService.login(this.loginForm.value);
+    this.router.navigateByUrl('/admin');
   }
 
 }
